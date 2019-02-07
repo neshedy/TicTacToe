76909bfa1699d35c86ad7da20efb7d277d42d953
neshedy <35135350+neshedy@users.noreply.github.com>
2019-02-07 19:41:15 +0800

    update game to half complete
    updated

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,18 +7,24 @@ import java.util.Scanner;
 class JavaApplication4 {
 
     char tttarray[][] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '},};
-    static int tempint = 1;
+    static boolean tempintone = true;
+    static boolean tempinttwo = true;
+    static boolean tempintwin = true;
     public static void main(String[] args) {
         JavaApplication4 newobj = new JavaApplication4();
         newobj.displayboard();
-        while (tempint == 1){
-            newobj.promptplayerone();
+        while (tempintwin == true) {
+            tempintone = true;
+            tempinttwo = true;
+            while (tempintone == true){
+                newobj.promptplayerone();
+            }
+            newobj.displayboard();
+            while (tempinttwo == true){
+                newobj.promptplayertwo();
+            }
+            newobj.displayboard();
         }
-        newobj.displayboard();
-        {
-            newobj.promptplayertwo();
-        }
-        newobj.displayboard();
     }
 
     public void displayboard() {
@@ -34,46 +40,65 @@ class JavaApplication4 {
     public void promptplayerone() {
         Scanner playeronexObj = new Scanner(System.in);
         Scanner playeroneyObj = new Scanner(System.in);
-        
         System.out.println("player one enter x coordinate");
-
         String playeronex = playeronexObj.nextLine();
         int playeronexint = Integer.parseInt(playeronex);
         System.out.println("player one y coordinate");
-        
         String playeroney = playeroneyObj.nextLine();
         int playeroneyint = Integer.parseInt(playeroney);
-        
+                if ((tttarray[0][0] == 'X' && tttarray[0][1] == 'X' && tttarray[0][2] == 'X') ||
+                (tttarray[1][0] == 'X' && tttarray[1][1] == 'X' && tttarray[1][2] == 'X') ||
+                (tttarray[2][0] == 'X' && tttarray[2][1] == 'X' && tttarray[2][2] == 'X') ||
+                (tttarray[0][0] == 'X' && tttarray[1][0] == 'X' && tttarray[2][0] == 'X') ||
+                (tttarray[0][1] == 'X' && tttarray[1][1] == 'X' && tttarray[2][1] == 'X') ||
+                (tttarray[0][2] == 'X' && tttarray[1][2] == 'X' && tttarray[2][2] == 'X') ||       
+                (tttarray[0][0] == 'X' && tttarray[1][1] == 'X' && tttarray[2][2] == 'X') ||
+                (tttarray[2][0] == 'X' && tttarray[1][1] == 'X' && tttarray[0][2] == 'X') )
+        {
+            tempintwin = false;
+            System.out.println("Player one wins");
+        }
         if (tttarray[playeronexint][playeroneyint]== 'X' ||
                 tttarray[playeronexint][playeroneyint] == 'O') {
-                tempint = 1;
+                tempintone = true;
+                System.out.println("Space taken");
         }
         else {
             tttarray[playeronexint][playeroneyint] = 'X';
-            tempint = 0;
+            tempintone = false;
         }
     }
+    
     public void promptplayertwo() {
         Scanner playertwoxObj = new Scanner(System.in);
         Scanner playertwoyObj = new Scanner(System.in);
-        
         System.out.println("player two enter x coordinate");
-
         String playertwox = playertwoxObj.nextLine();
         int playertwoxint = Integer.parseInt(playertwox);
         System.out.println("player two y coordinate");
-        
         String playertwoy = playertwoyObj.nextLine();
         int playertwoyint = Integer.parseInt(playertwoy);
-       
+        if ((tttarray[0][0] == 'O' && tttarray[0][1] == 'O' && tttarray[0][2] == 'O') ||
+                (tttarray[1][0] == 'O' && tttarray[1][1] == 'O' && tttarray[1][2] == 'O') ||
+                (tttarray[2][0] == 'O' && tttarray[2][1] == 'O' && tttarray[2][2] == 'O') ||
+                (tttarray[0][0] == 'O' && tttarray[1][0] == 'O' && tttarray[2][0] == 'O') ||
+                (tttarray[0][1] == 'O' && tttarray[1][1] == 'O' && tttarray[2][1] == 'O') ||
+                (tttarray[0][2] == 'O' && tttarray[1][2] == 'O' && tttarray[2][2] == 'O') ||       
+                (tttarray[0][0] == 'O' && tttarray[1][1] == 'O' && tttarray[2][2] == 'O') ||
+                (tttarray[2][0] == 'O' && tttarray[1][1] == 'O' && tttarray[0][2] == 'O') )
+        {
+            tempintwin = false;
+            System.out.println("Player two wins");
+        }
+                        
         if (tttarray[playertwoxint][playertwoyint] == 'X'
                 || tttarray[playertwoxint][playertwoyint] == 'O'){
-            tempint = 1;
+            tempinttwo = true;
+            System.out.println("Space taken");
         }
-        else
-        {
+        else {
             tttarray[playertwoxint][playertwoyint] = 'O';
-            tempint = 0;
+            tempinttwo = false;
         }
     }
 }
