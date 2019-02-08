e0538bc3980ed19f5df699c06b25c58ad059a085
neshedy <35135350+neshedy@users.noreply.github.com>
2019-02-08 08:39:28 +0800

    Create TicTacToe.cs
    A Simple TicTacToe program written in C#

---
+++ TicTacToe.cs
@@ -0,0 +1,14 @@
+//A simple TicTacToe program written in c#
+using System;
+namespace Program
+{
+    public class myClass 
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
