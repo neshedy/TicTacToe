370b4de15c6e43141d12daaa67352d62bea3c928
neshedy <35135350+neshedy@users.noreply.github.com>
2019-02-12 08:31:39 +0800

    Update TicTacToe.cpp
    -Incorporated classes
    -Got rid of goto

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,171 +2,114 @@
 
 using namespace std;
 
-int tttarray [3][3] = {{0,0,0},{0,0,0},{0,0,0}};
-int playeroney, playeronex, playertwoy, playertwox;
-
-class tttClass {
+class MyClass {
 	public:
-		void playeronestart() {
-			cout << "Player one start" << endl;
+	char tttarray[3][3] = {{'-','-','-',},{'-','-','-',},{'-','-','-',},};
+	int playeronex, playeroney, playertwox, playertwoy;
+	
+	void DisplayBoard(){
+	for (int i = 0; i < 3; i ++) {
+			for (int j = 0; j < 2; j++) {
+				cout << tttarray[i][j];
+			}
+			cout << tttarray[i][2] << endl;
 		}
-		void playeronex() {
-			cout << "Playerone enter x coord" << endl;
+	}
+	
+	void PlayerOne(){
+		cout << "Player one enter x coordinate" << endl;
+		cin >> playeronex;
+		cout << "Player one enter y coordinate" << endl;
+		cin >> playeroney;
+	}
+	
+	void PlayerTwo(){
+		cout << "Player two enter x coordinate" << endl;
+		cin >> playertwox;
+		cout << "Player two enter y coordinate" << endl;
+		cin >> playertwoy;
+	}
+	
+	bool CheckWinOne(){
+		if (tttarray[0][0] == 'X' && tttarray[0][1] == 'X' && tttarray[0][2] == 'X' ||
+			tttarray[1][0] == 'X' && tttarray[1][1] == 'X' && tttarray[1][2] == 'X' ||
+			tttarray[2][0] == 'X' && tttarray[2][1] == 'X' && tttarray[2][2] == 'X' ||
+			tttarray[0][0] == 'X' && tttarray[1][0] == 'X' && tttarray[2][0] == 'X' ||
+			tttarray[0][1] == 'X' && tttarray[1][1] == 'X' && tttarray[2][1] == 'X' ||
+			tttarray[0][2] == 'X' && tttarray[1][2] == 'X' && tttarray[2][2] == 'X' ||
+			tttarray[0][0] == 'X' && tttarray[1][1] == 'X' && tttarray[2][2] == 'X' ||
+			tttarray[0][2] == 'X' && tttarray[1][1] == 'X' && tttarray[2][0] == 'X') {
+				
+				return true;
+				cout << "Playerone wins!" << endl;
+			}
+			else 
+				return false;
+	}
+	
+	bool CheckWinTwo(){
+		if (tttarray[0][0] == 'O' && tttarray[0][1] == 'O' && tttarray[0][2] == 'O' ||
+			tttarray[1][0] == 'O' && tttarray[1][1] == 'O' && tttarray[1][2] == 'O' ||
+			tttarray[2][0] == 'O' && tttarray[2][1] == 'O' && tttarray[2][2] == 'O' ||
+			tttarray[0][0] == 'O' && tttarray[1][0] == 'O' && tttarray[2][0] == 'O' ||
+			tttarray[0][1] == 'O' && tttarray[1][1] == 'O' && tttarray[2][1] == 'O' ||
+			tttarray[0][2] == 'O' && tttarray[1][2] == 'O' && tttarray[2][2] == 'O' ||
+			tttarray[0][0] == 'O' && tttarray[1][1] == 'O' && tttarray[2][2] == 'O' ||
+			tttarray[0][2] == 'O' && tttarray[1][1] == 'O' && tttarray[2][0] == 'O') {
+				cout << "Playertwo wins!" << endl;
+				return true;
+			}
+			else
+				return false;
+	}
+	
+	bool CheckSpaceOne(){
+		if (tttarray[playeronex][playeroney] == 'X' || tttarray[playeronex][playeroney] == 'O') {
+			cout << "Error space taken" << endl;
+			return true;
 		}
-		void playeroney() {
-			cout << "Playerone enter y coord" << endl;
+		else
+			return false;
+	}
+	
+	bool CheckSpaceTwo(){
+		if (tttarray[playertwox][playertwoy] == 'X' || tttarray[playertwox][playertwoy] == 'O') {
+			cout << "Error space taken" << endl;
+			return true;
 		}
-		void Error() {
-			cout << "Space Taken" << endl;
-		}
-		void playertwostart() {
-			cout << "Player two start" << endl;
-		}
-		void playertwox() {
-			cout << "Playertwo enter x coord" << endl;
-		}
-		void playertwoy() {
-			cout << "Playertwo enter y coord" << endl;
-		}
-		void playeronewin() {
-			cout << "Player One wins" << endl;
-		}
-		void playertwowin() {
-			cout << "Player Two wins" << endl;
-		}
-		void coutArr() {
-		cout << tttarray[0][0] << tttarray [0][1] << tttarray [0][2] << endl;
-		cout << tttarray[1][0] << tttarray [1][1] << tttarray [1][2] << endl;
-		cout << tttarray[2][0] << tttarray [2][1] << tttarray [2][2] << endl;
-		}
+		else
+			return false;
+	}
 };
 
-int checkwinone() {
-	if 		((tttarray[0][0] == 1 && tttarray [0][1] == 1 && tttarray [0][2] == 1)
-			|| (tttarray[1][0] == 1 && tttarray [1][1] == 1 && tttarray [1][2] == 1)
-			|| (tttarray[2][0] == 1 && tttarray [2][1] == 1 && tttarray [2][2] == 1)
-			|| (tttarray[0][0] == 1 && tttarray [1][0] == 1 && tttarray [2][0] == 1)
-			|| (tttarray[0][1] == 1 && tttarray [1][1] == 1 && tttarray [2][1] == 1)
-			|| (tttarray[0][2] == 1 && tttarray [1][2] == 1 && tttarray [2][2] == 1)
-			|| (tttarray[0][0] == 1 && tttarray [1][1] == 1 && tttarray [2][2] == 1)
-			|| (tttarray[0][2] == 1 && tttarray [1][1] == 1 && tttarray [2][0] == 1))
-	return 1;
-	else 
-	return 0;
-}
-
-int checkwintwo() {
-	if 		((tttarray[0][0] == 2 && tttarray [0][1] == 2 && tttarray [0][2] == 2)
-			|| (tttarray[1][0] == 2 && tttarray [1][1] == 2 && tttarray [1][2] == 2)
-			|| (tttarray[2][0] == 2 && tttarray [2][1] == 2 && tttarray [2][2] == 2)
-			|| (tttarray[0][0] == 2 && tttarray [1][0] == 2 && tttarray [2][0] == 2)
-			|| (tttarray[0][1] == 2 && tttarray [1][1] == 2 && tttarray [2][1] == 2)
-			|| (tttarray[0][2] == 2 && tttarray [1][2] == 2 && tttarray [2][2] == 2)
-			|| (tttarray[0][0] == 2 && tttarray [1][1] == 2 && tttarray [2][2] == 2)
-			|| (tttarray[0][2] == 2 && tttarray [1][1] == 2 && tttarray [2][0] == 2))
-	return 1;
-	else 
-	return 0;
-}
-
-int checkgameend() {
-	if 		((tttarray[0][0] != 0 && tttarray [0][1] != 0 && tttarray [0][2] != 0)
-			&& (tttarray[1][0] != 0 && tttarray [1][1] != 0 && tttarray [1][2] != 0)
-			&& (tttarray[2][0] != 0 && tttarray [2][1] != 0 && tttarray [2][2] != 0)
-			&& (tttarray[0][0] != 0 && tttarray [1][0] != 0 && tttarray [2][0] != 0)
-			&& (tttarray[0][1] != 0 && tttarray [1][1] != 0 && tttarray [2][1] != 0)
-			&& (tttarray[0][2] != 0 && tttarray [1][2] != 0 && tttarray [2][2] != 0)
-			&& (tttarray[0][0] != 0 && tttarray [1][1] != 0 && tttarray [2][2] != 0)
-			&& (tttarray[0][2] != 0 && tttarray [1][1] != 0 && tttarray [2][0] != 0))
-		return 1;
-	else
-		return 0;
-}
-
-int checkerrorone () {
-	if (tttarray[playeronex][playeroney] == 1 || tttarray[playeronex][playeroney] == 2)
-		return 1;
-	else 
-		return 0;
-}
-
-int checkerrortwo () {
-	if (tttarray[playertwox][playertwoy] == 2 || tttarray[playertwox][playertwoy] == 1)
-		return 1;
-	else
-		return 0;
-}
-
 int main() {
-	tttClass tttObject;
-	tttObject.coutArr();
-
-	game:
-	tttObject.playeronestart();
-	
-	playeroneplay:
-	tttObject.playeronex();
-	cin >> playeronex;
-	tttObject.playeroney();
-	cin >> playeroney;
-	
-	int errorresultone;
-	errorresultone = checkerrorone();
-	if (errorresultone == 1)
-		{
-			tttObject.Error();
-			goto playeroneplay;
+	MyClass MyObject;
+	bool shouldplay = true;
+	while(shouldplay == true){
+		MyObject.DisplayBoard();
+		bool playone = true;
+		while (playone == true) {
+			MyObject.PlayerOne();
+		if (MyObject.CheckSpaceOne() == false)
+			playone = false;
 		}
-	else
-		tttarray [playeronex][playeroney] = 1;
-	tttObject.coutArr();
-	int winresult;
-	winresult = checkwinone();
-	if 	(winresult == 1)	
-	{
-		tttObject.playeronewin();
-		int test;
-		cin >> test;
-		return 0;
+		MyObject.tttarray[MyObject.playeronex][MyObject.playeroney] = 'X';
+		if (MyObject.CheckWinOne() == true) {
+			shouldplay = false;
+			break;
+		}
+		
+		MyObject.DisplayBoard();
+		bool playtwo = true;
+		while (playtwo == true) {
+			MyObject.PlayerTwo();
+		if (MyObject.CheckSpaceTwo() == false)
+			playtwo = false;
+		}
+		MyObject.tttarray[MyObject.playertwox][MyObject.playertwoy] = 'O';
+		if (MyObject.CheckWinTwo() == true) {
+			shouldplay = false;
+			break;
+		}
 	}
-	int gameendresult;
-	gameendresult = checkgameend();
-	if (gameendresult == 1) {
-		int tempend;
-		cin >> tempend;
-		return 0;
-	}
-	tttObject.playertwostart();
-	
-	playertwoplay:
-	tttObject.playertwox();
-	cin >> playertwox;
-	tttObject.playertwoy();
-	cin >> playertwoy;
-	
-	int errorresulttwo;
-	errorresulttwo = checkerrortwo();
-	if (errorresulttwo == 1)
-	{
-		tttObject.Error();
-		goto playertwoplay;
-	}
-	else
-	tttarray [playertwox][playertwoy] = 2;
-	tttObject.coutArr();
-	winresult = checkwintwo();
-	if (winresult == 1)
-	{
-		tttObject.playertwowin();
-		int test;
-		cin >> test;
-		return 0;
-	}
-	gameendresult = checkgameend();
-	if (gameendresult == 1) {
-		int tempend;
-		cin >> tempend;
-		return 0;
-	}
-	goto game;
 }
