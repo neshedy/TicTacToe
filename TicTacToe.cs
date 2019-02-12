f5425846a48acc84dbdd03ec779b97a2345e9796
neshedy <35135350+neshedy@users.noreply.github.com>
2019-02-12 17:11:33 +0800

    Update TicTacToe.cs
    much clearer coding
    no goto statements

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -1,131 +1,135 @@
-// A TicTacToe program in C#.
 using System;
-namespace Program
-{
-    public class myClass 
-    {
-		char[,] tttarray = {{'-','-','-'},{'-','-','-'},{'-','-','-'}};
-		int playeronex, playeroney, playertwox, playertwoy;
-		public static bool shouldplay = true;
-		public static bool checkwin = false;
-		public static bool checkdraw = false;
-		
-        public static void Main(string[] args) 
-        {
-			myClass myObj = new myClass();
-			
-            Console.WriteLine("Hello World!");
-			myObj.displayboard();
-			while (shouldplay == true){
-				myObj.playerone();
-				if (checkwin == true)
-				goto end;
-				if (checkdraw == true)
-				goto end;
-				myObj.displayboard();
-				myObj.playertwo();
-				if (checkwin == true)
-				goto end;
-				if (checkdraw == true)
-				goto end;
-				myObj.displayboard();
-			}
-			end:
-			myObj.displayboard();
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-        }
-		
-		public void playerone() {
-			playeronestart:
-			Console.WriteLine("PlayerOne Enter X Coord");
-			string playeronexstring = Console.ReadLine();
-			Console.WriteLine("PlayerOne Enter Y Coord");
-			string playeroneystring = Console.ReadLine();
-			playeronex = Int32.Parse(playeronexstring);
-			playeroney = Int32.Parse(playeroneystring);
-			if (tttarray[playeronex, playeroney] == 'X' || tttarray[playeronex, playeroney] == 'O') {
-				Console.WriteLine("Space Taken");
-				goto playeronestart;
-			}
-			tttarray[playeronex,playeroney] = 'X';
-		if (tttarray[0,0] != '-' && 
-		tttarray[0,1] != '-' &&
-		tttarray[0,2] != '-' &&
-		tttarray[1,0] != '-' &&
-		tttarray[1,1] != '-' &&
-		tttarray[1,2] != '-' &&
-		tttarray[2,0] != '-' &&
-		tttarray[2,1] != '-' &&
-		tttarray[2,2] != '-') {
-			Console.WriteLine("Draw");
-			checkdraw = true;
-		}
-		if ((tttarray[0,0] == 'X' && tttarray [0,1] == 'X' && tttarray[0,2] == 'X') ||
-			(tttarray[1,0] == 'X' && tttarray [1,1] == 'X' && tttarray[1,2] == 'X') ||
-			(tttarray[2,0] == 'X' && tttarray [2,1] == 'X' && tttarray[2,2] == 'X') ||
-			(tttarray[0,0] == 'X' && tttarray [1,0] == 'X' && tttarray[2,0] == 'X') ||
-			(tttarray[0,1] == 'X' && tttarray [1,1] == 'X' && tttarray[2,1] == 'X') ||
-			(tttarray[0,2] == 'X' && tttarray [1,2] == 'X' && tttarray[2,2] == 'X') ||
-			(tttarray[0,0] == 'X' && tttarray [1,1] == 'X' && tttarray[2,2] == 'X') ||
-			(tttarray[2,0] == 'X' && tttarray [1,1] == 'X' && tttarray[0,2] == 'X')) {
-				Console.WriteLine("PlayerOne Wins");
-				checkwin = true;
-				shouldplay = false;
-			}
-			
-		}
-		
-		public void playertwo() {
-			playertwostart:
-			Console.WriteLine("PlayerTwo Enter X Coord");
-			string playertwoxstring = Console.ReadLine();
-			Console.WriteLine("PlayerTwo Enter Y Coord");
-			string playertwoystring = Console.ReadLine();
-			playertwox = Int32.Parse(playertwoxstring);
-			playertwoy = Int32.Parse(playertwoystring);
-			if (tttarray[playertwox, playertwoy] == 'X' || tttarray[playertwox, playertwoy] == 'O') {
-				Console.WriteLine("Space Taken");
-				goto playertwostart;
-			}
-			tttarray[playertwox,playertwoy] = 'O';
-		if (tttarray[0,0] != '-' && 
-		tttarray[0,1] != '-' &&
-		tttarray[0,2] != '-' &&
-		tttarray[1,0] != '-' &&
-		tttarray[1,1] != '-' &&
-		tttarray[1,2] != '-' &&
-		tttarray[2,0] != '-' &&
-		tttarray[2,1] != '-' &&
-		tttarray[2,2] != '-') {
-			Console.WriteLine("Draw");
-			checkdraw = true;
-		}
-			
-		if ((tttarray[0,0] == 'O' && tttarray [0,1] == 'O' && tttarray[0,2] == 'O') ||
-			(tttarray[1,0] == 'O' && tttarray [1,1] == 'O' && tttarray[1,2] == 'O') ||
-			(tttarray[2,0] == 'O' && tttarray [2,1] == 'O' && tttarray[2,2] == 'O') ||
-			(tttarray[0,0] == 'O' && tttarray [1,0] == 'O' && tttarray[2,0] == 'O') ||
-			(tttarray[0,1] == 'O' && tttarray [1,1] == 'O' && tttarray[2,1] == 'O') ||
-			(tttarray[0,2] == 'O' && tttarray [1,2] == 'O' && tttarray[2,2] == 'O') ||
-			(tttarray[0,0] == 'O' && tttarray [1,1] == 'O' && tttarray[2,2] == 'O') ||
-			(tttarray[2,0] == 'O' && tttarray [1,1] == 'O' && tttarray[0,2] == 'O')) {
-				Console.WriteLine("PlayerTwo Wins");
-				checkwin = true;
-				shouldplay = false;
-			}
-		}
-		
-		public void displayboard() {
-			Console.WriteLine("___");
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
+
+namespace MyNamespace {
+	
+	public class MyClass{
+	public static char[,]tttarray = {{'-','-','-'},{'-','-','-'},{'-','-','-'}};
+	public static int playeronex, playeroney, playertwox, playertwoy;
+		public void DisplayBoard(){
+			for (int i = 0; i < 3; i++){
+				for (int j = 0; j < 2; j++){
 					Console.Write(tttarray[i,j]);
 				}
-				Console.WriteLine("");
+				Console.WriteLine(tttarray[i,2]);
 			}
-			Console.WriteLine("___");
 		}
 		
-    }
+		public void PlayerOne(){
+			bool playeroneplay = true;
+			while(playeroneplay == true) {
+				Console.WriteLine("Player one enter x coordinate");
+				playeronex = Int32.Parse(Console.ReadLine());
+				Console.WriteLine("Player one enter y coordinate");
+				playeroney = Int32.Parse(Console.ReadLine());
+				if(CheckSpaceOne() == false) {
+					tttarray[playeronex,playeroney] = 'X';
+					break;
+				}
+			}
+		}
+		
+		public void PlayerTwo(){
+			bool playertwoplay = true;
+			while(playertwoplay == true) {
+				Console.WriteLine("Player two enter x coordinate");
+				playertwox = Int32.Parse(Console.ReadLine());
+				Console.WriteLine("Player two enter y coordinate");
+				playertwoy = Int32.Parse(Console.ReadLine());
+				if(CheckSpaceTwo() == false) {
+					tttarray[playertwox,playertwoy] = 'O';
+					break;
+				}
+			}
+		}
+		
+		public bool CheckSpaceOne() {
+			if(tttarray[playeronex,playeroney] == 'X' || tttarray[playeronex,playeroney] == 'O') {
+				Console.WriteLine("Space taken");
+				return true;
+			}
+			else
+				return false;
+		}
+		
+		public bool CheckSpaceTwo() {
+			if(tttarray[playertwox,playertwoy] == 'X' || tttarray[playertwox,playertwoy] == 'O') {
+				Console.WriteLine("Space taken");
+				return true;
+			}
+			else
+				return false;
+		}
+		
+		public bool CheckWinOne() {
+			if(tttarray[0,0] == 'X' && tttarray[0,1] == 'X' && tttarray[0,2] == 'X' ||
+				tttarray[1,0] == 'X' && tttarray[1,1] == 'X' && tttarray[1,2] == 'X' ||
+				tttarray[2,0] == 'X' && tttarray[2,1] == 'X' && tttarray[2,2] == 'X' ||
+				tttarray[0,0] == 'X' && tttarray[1,0] == 'X' && tttarray[2,0] == 'X' ||
+				tttarray[0,1] == 'X' && tttarray[1,1] == 'X' && tttarray[2,1] == 'X' ||
+				tttarray[0,2] == 'X' && tttarray[1,2] == 'X' && tttarray[2,2] == 'X' ||
+				tttarray[0,0] == 'X' && tttarray[1,1] == 'X' && tttarray[2,2] == 'X' ||
+				tttarray[0,2] == 'X' && tttarray[1,1] == 'X' && tttarray[2,0] == 'X' ) {
+				Console.WriteLine("Playerone wins");
+				return true;
+				}
+			else 
+				return false;
+		}
+		
+		public bool CheckWinTwo() {
+			if(tttarray[0,0] == 'O' && tttarray[0,1] == 'O' && tttarray[0,2] == 'O' ||
+				tttarray[1,0] == 'O' && tttarray[1,1] == 'O' && tttarray[1,2] == 'O' ||
+				tttarray[2,0] == 'O' && tttarray[2,1] == 'O' && tttarray[2,2] == 'O' ||
+				tttarray[0,0] == 'O' && tttarray[1,0] == 'O' && tttarray[2,0] == 'O' ||
+				tttarray[0,1] == 'O' && tttarray[1,1] == 'O' && tttarray[2,1] == 'O' ||
+				tttarray[0,2] == 'O' && tttarray[1,2] == 'O' && tttarray[2,2] == 'O' ||
+				tttarray[0,0] == 'O' && tttarray[1,1] == 'O' && tttarray[2,2] == 'O' ||
+				tttarray[0,2] == 'O' && tttarray[1,1] == 'O' && tttarray[2,0] == 'O' ) {
+					Console.WriteLine("Playertwo wins");
+					return true;
+				}
+				else
+					return false;
+		}
+		
+		public bool CheckDraw() {
+			if(tttarray[0,0] != '-' && tttarray[0,1] != '-' && tttarray[0,2] != '-'
+			&& tttarray[1,0] != '-' && tttarray[1,1] != '-' && tttarray[1,2] != '-'
+			&& tttarray[2,0] != '-' && tttarray[2,1] != '-' && tttarray[2,2] != '-') {
+				Console.WriteLine("Draw");
+				return true;
+			}
+			else
+				return false;
+		}
+	};
+	
+	class TestClass{
+		public static void Main(string[] args){
+			MyClass MyObject = new MyClass();
+			bool shouldplay = true;
+			while (shouldplay == true) {
+				MyObject.DisplayBoard();
+				MyObject.PlayerOne();
+				if (MyObject.CheckWinOne() == true) {
+					MyObject.DisplayBoard();
+					break;
+				}
+				else if (MyObject.CheckDraw() == true) {
+					MyObject.DisplayBoard();
+					break;
+				}
+				MyObject.DisplayBoard();
+				MyObject.PlayerTwo();
+				if (MyObject.CheckWinTwo() == true) {
+					MyObject.DisplayBoard();
+					break;
+				}
+				else if (MyObject.CheckDraw() == true) {
+					MyObject.DisplayBoard();
+					break;
+				}
+			}
+		}
+	}
 }
